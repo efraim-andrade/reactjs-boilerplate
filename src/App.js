80a5047308b87f27e06c61980c1c3ff7bbb238e3
Efraim Andrade <efraim.dev@gmail.com>
2019-07-22 12:10:25 -0300

    feat: (structure)
    added eslint, editorconfig, absolutepaths and routes configuration.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import Routes from '~/routes';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <Routes />;
 }
